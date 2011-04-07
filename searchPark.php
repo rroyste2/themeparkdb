df774e0200c12a486b8cef43ed0d4bd02d308b19
mikeyGlitz <mglitzos@mail.umw.edu>
2011-04-06 23:56:17 -0400

    I added the search page to the repository

--- searchPark.php
+++ searchPark.php
@@ -23,71 +23,70 @@ License: Creative Commons Attribution
 </head>
 
 <body class="noheader">
-	<?php
-		include 'container.php';
-	?> 
-    <div id="body">            
+<div id="container">
+        <div id="header">
+        <h1><a href="/">Themepark<strong>Database</strong></a></h1>
+        <h2>The ultimate source for themepark and coaster information.</h2>
+
+
+        <div class="clear"></div>
+    </div>
+    <div id="nav">
+        <ul class="sf-menu dropdown">
+                <li><a href="index.php">Home</a></li>
+            <li><a class="has_submenu" href="parks.php">Parks</a>
+
+            </li>
+            <li><a class="has_submenu" href="rides.php">Rides</a>
+
+            </li>
+            <li class="selected"><a class="has_submenu" href="searchPark.php">Search Parks</a>
+                        <ul>
+                        <li><a href="#">For coasters</a></li>
+                    <li><a href="#">For water rides</a></li>
+                    <li><a href="#">For family rides</a></li>
+                </ul>
+            </li>
+
+
+
+            <li><a class="has_submenu" href="login.php">User Panel</a>
+                                <ul>
+                <!-- These are the default options if there is no user logged in -->
+                    <li><a href="login.html">Log On</a></li>
+                    <li><a href="register.html">Register</a></li>
+                                        <li><a href="logout.php">Log Off</a></li>
+                </ul>
+
+                                <?php
+if(isset($_SESSION['username'])){?>
+                <li><a class="has_submenu" href="index.php">Edit Parks</a>
+                        <ul>
+                        <li><a href="addPark.html">Add Park</a></li>
+                    <li><a href="deletePark.html">Delete park</a></li>
+                </ul>
+            </li>
+                        <?php
+                        }
+                        ?>
+
+            </li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+
+        </div>
+	<div id="body">            
 		<div id="content">
             <div class="box">
-		<?php
-			$_SESSION ['type'] = $type;
-
-echo $_SESSION ['type'];
- $type = $_SESSION['type'];
-  include "db_connect.php";
-  if (isset($_POST['search']))
-  {
-        if ($type == 'Rollercoaster')
-        {
-                $query = "SELECT * FROM rollercoaster";
-                //mysql_real_escape_string($movies);
-
-                echo $query;
-
-                $result = mysqli_query($db, $query)
-                        or die("Error Querying Database");
-                echo "<table id=\"hor-minimalist-b\">\n<tr><th>Movie</th><th>Theater</th><tr>\n\n";
-        }else
-        echo "else";
-
-  }
-		?>
+		<form action="searchPark.php" method="get">
+			Enter a Query: <input type=text name=query /><input type=submit value="Search" />
+		</form><br /><hr />
+		<!--TODO: Query Code -->
             </div>
         </div>
-        
-        <div class="sidebar">
-            <ul>	
-               <li>
-                    <h4><span>Our <strong>Pages</strong></span></h4>
-                    <ul class="blocklist">
-                        <li><a href="index.php">Home</a></li>
-                        <li><a href="examples.html">Examples</a></li>
-                        <li><a href="#">Products</a></li>
-                        <li><a href="#">Solutions</a></li>
-                        <li><a href="#">Contact</a></li>
-                    </ul>
-                </li>
-                
-                <li>
-                    <h4><span>About <strong>Us</strong></span></h4>
-                    <ul>
-                        <li>
-                        	<p style="margin: 0;">Aenean nec massa a tortor auctor sodales sed a dolor. Duis vitae lorem sem. Proin at velit vel arcu pretium luctus.</p>
-                        </li>
-                    </ul>
-                </li>
-                
-                <li>
-                    <h4 class="h4"><span>Cool <strong>Sites</strong></span></h4>
-                    <ul>
-                        <li><a href="http://www.themeforest.net/?ref=spykawg" title="premium templates"><strong>ThemeForest</strong></a> - premium HTML templates, WordPress themes and PHP scripts</li>
-                        <li><a href="http://www.dreamhost.com/r.cgi?259541" title="web hosting"><strong>Web hosting</strong></a> - 50 dollars off when you use promocode <strong>awesome50</strong></li>
-                        <li><a href="http://www.4templates.com/?aff=spykawg" title="4templates"><strong>4templates</strong></a> - brilliant premium templates</li>
-                    </ul>
-                </li>
-                
-            </ul> 
-        </div>
+        <?PHP
+		include'sidebar.php';
+	?>
     	<div class="clear"></div>
     </div>
 </div>
